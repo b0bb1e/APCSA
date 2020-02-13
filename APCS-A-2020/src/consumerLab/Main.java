bddfaad5a1d3ce14a89a6be17997a44c8f2723e0
okamotof7379 <okamotof7379@CA-SU-F106-40.sduhsd.lan>
2020-02-13 11:12:32 -0800

    start of consumer lab from school

---
+++ APCS-A-2020/src/consumerLab/Main.java
@@ -0,0 +1,10 @@
+package consumerLab;
+
+import java.io.File;
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		System.out.println(Review.totalSentiment("src/consumerLab/26WestReview.txt"));
+	}
+}
