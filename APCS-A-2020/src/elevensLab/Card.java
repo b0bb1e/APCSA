f1405fa03570cf0abe4c7c4178cc0b417f222bba
okamotof7379 <okamotof7379@CA-SU-F106-40.sduhsd.lan>
2020-02-27 11:08:49 -0800

    adding in extra reqs

--- APCS-A-2020/src/elevensLab/Card.java
+++ APCS-A-2020/src/elevensLab/Card.java
@@ -9,27 +9,38 @@ public class Card
 			"FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING"};
 
 	private String suit;
-	private int face;
+	private int face, pointValue;
 
-  	//constructors
 	public Card(String s, int value) {
 		suit = s;
 		face = value;
+		pointValue = 0;
+	}
+	
+	public Card(String s, int value, int points) {
+		suit = s;
+		face = value;
+		pointValue = points;
 	}
 
 	public void setSuit(String s) {suit = s;}
 	
 	public void setFace(int value) {face = value;}
+	
+	public void setPointValue(int points) {pointValue = points;}
 
 	public String getSuit() {return suit;}
 	
 	public int getFace() {return face;}
+	
+	public int getPointValue() {return pointValue;}
 	
 	public boolean isEqual(Card check) {
 		return check.getSuit() == suit && check.getFace() == face;
 	}
 
 	public String toString() {
-		return FACES[face - 1] + " of " + suit + " (point value = " + face + ")";
+		if (pointValue == 0) return FACES[face - 1] + " of " + suit + " (point value = " + face + ")";
+		return FACES[face - 1] + " of " + suit + " (point value = " + pointValue + ")";
 	}
 }
